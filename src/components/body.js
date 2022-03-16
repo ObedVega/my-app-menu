6e83d2242575b3c7ee35bc5189ac932ed4b080e9
Obed Vega <vega.obed@gmail.com>
2022-03-16 16:33:34 -0700

    first dev

---
+++ src/components/body.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Body = () => {
+  return (
+    <>
+    <div>
+        <h1>hola mundo</h1>
+    </div>
+    
+    </>
+  );
+};
---
+++ src/components/navbar.js
@@ -0,0 +1,29 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.css';
+import Container from 'react-bootstrap/Container';
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+ 
+export const Menu = () => {
+  return (
+   <>
+    <Navbar bg="dark" variant="dark">
+    <Container>
+      <Navbar.Brand href="#home">
+        <img
+          alt=""
+          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png"
+          width="30"
+          height="30"
+          className="d-inline-block align-top"
+        />{' '}
+      RAGA Urban Cafe
+      </Navbar.Brand>
+      <Nav className="me-auto">
+        <Nav.Link href="#home">Home</Nav.Link>
+        </Nav>
+    </Container>
+  </Navbar>
+  </>
+  );
+};
