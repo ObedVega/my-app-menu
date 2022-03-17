386ce1dc9864e14a150c2d3c96e16eff7d81315d
Obed Vega <vega.obed@gmail.com>
2022-03-16 17:03:41 -0700

    dev2

--- src/components/foot.js
+++ src/components/foot.js
@@ -19,30 +19,30 @@ export const Footer = () => {
       <div className="col-6 col-md">
         <h5>Features</h5>
         <ul className="list-unstyled text-small">
-          <li className="mb-1"><a className="link-secondary text-decoration-none" href="#">Cool stuff</a></li>
-          <li className="mb-1"><a className="link-secondary text-decoration-none" href="#">Random feature</a></li>
-          <li className="mb-1"><a className="link-secondary text-decoration-none" href="#">Team feature</a></li>
-          <li className="mb-1"><a className="link-secondary text-decoration-none" href="#">Stuff for developers</a></li>
-          <li className="mb-1"><a className="link-secondary text-decoration-none" href="#">Another one</a></li>
-          <li className="mb-1"><a className="link-secondary text-decoration-none" href="#">Last time</a></li>
+          <li className="mb-1"><a className="link-secondary text-decoration-none" href="#a">Cool stuff</a></li>
+          <li className="mb-1"><a className="link-secondary text-decoration-none" href="#a">Random feature</a></li>
+          <li className="mb-1"><a className="link-secondary text-decoration-none" href="#a">Team feature</a></li>
+          <li className="mb-1"><a className="link-secondary text-decoration-none" href="#a">Stuff for developers</a></li>
+          <li className="mb-1"><a className="link-secondary text-decoration-none" href="#a">Another one</a></li>
+          <li className="mb-1"><a className="link-secondary text-decoration-none" href="#j">Last time</a></li>
         </ul>
       </div>
       <div className="col-6 col-md">
         <h5>Resources</h5>
         <ul className="list-unstyled text-small">
-          <li className="mb-1"><a className="link-secondary text-decoration-none" href="#">Resource</a></li>
-          <li className="mb-1"><a className="link-secondary text-decoration-none" href="#">Resource name</a></li>
-          <li className="mb-1"><a className="link-secondary text-decoration-none" href="#">Another resource</a></li>
-          <li className="mb-1"><a className="link-secondary text-decoration-none" href="#">Final resource</a></li>
+          <li className="mb-1"><a className="link-secondary text-decoration-none" href="#a">Resource</a></li>
+          <li className="mb-1"><a className="link-secondary text-decoration-none" href="#a">Resource name</a></li>
+          <li className="mb-1"><a className="link-secondary text-decoration-none" href="#a">Another resource</a></li>
+          <li className="mb-1"><a className="link-secondary text-decoration-none" href="#a">Final resource</a></li>
         </ul>
       </div>
       <div className="col-6 col-md">
         <h5>About</h5>
         <ul className="list-unstyled text-small">
-          <li className="mb-1"><a className="link-secondary text-decoration-none" href="#">Team</a></li>
-          <li className="mb-1"><a className="link-secondary text-decoration-none" href="#">Locations</a></li>
-          <li className="mb-1"><a className="link-secondary text-decoration-none" href="#">Privacy</a></li>
-          <li className="mb-1"><a className="link-secondary text-decoration-none" href="#">Terms</a></li>
+          <li className="mb-1"><a className="link-secondary text-decoration-none" href="#a">Team</a></li>
+          <li className="mb-1"><a className="link-secondary text-decoration-none" href="#a">Locations</a></li>
+          <li className="mb-1"><a className="link-secondary text-decoration-none" href="#a">Privacy</a></li>
+          <li className="mb-1"><a className="link-secondary text-decoration-none" href="#a">Terms</a></li>
         </ul>
       </div>
     </div>
